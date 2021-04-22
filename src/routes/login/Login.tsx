208fb1269ccb40e8316d85a8146a900e31f6c0eb
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-22 23:23:45 +0300

    add: login screen styles

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './login.scss';
 import { loginValidator } from 'src/helpers/joiValidators';
+import instagramLogo from 'src/images/instagram-login.png';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [inputError, setInputError] = useState('');
+
+  useEffect(() => {
+    document.title = 'Login • Instagram';
+  }, []);
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -19,20 +24,47 @@ const Login = () => {
 
   return (
     <section className="login-container">
+      <img className="w-40" src={instagramLogo} alt="instagram" />
       <form onSubmit={(e) => handleLogin(e)}>
-        <input
-          type="text"
-          placeholder="username"
-          onChange={(event) => setUsername(event.target.value)}
-          onFocus={() => setInputError('')}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(event) => setPassword(event.target.value)}
-          onFocus={() => setInputError('')}
-        />
-        <button type="submit">Login</button>
+        <label className="relative w-72" htmlFor="username">
+          <span
+            className={`absolute text-gray-400 ${
+              username ? 'top-0 left-2 text-xs' : 'top-3 left-4 text-sm'
+            }`}
+          >
+            username
+          </span>
+          <input
+            className="login-input"
+            type="text"
+            id="username"
+            onChange={(event) => setUsername(event.target.value)}
+            onFocus={() => {
+              setInputError('');
+            }}
+          />
+        </label>
+        <label className="relative w-72 mt-2" htmlFor="password">
+          <span
+            className={`absolute text-gray-400 ${
+              password ? 'top-0 left-2 text-xs' : 'top-3 left-4 text-sm'
+            }`}
+          >
+            password
+          </span>
+          <input
+            className="login-input"
+            type="password"
+            id="password"
+            onChange={(event) => setPassword(event.target.value)}
+            onFocus={() => {
+              setInputError('');
+            }}
+          />
+        </label>
+        <button className="w-full" type="submit">
+          Login
+        </button>
       </form>
       {inputError}
     </section>
