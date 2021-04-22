a4eb7942e507ca8d5566925bd20a5c98f6692831
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-22 21:04:50 +0300

    add: login page inputs

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './login.scss';
 
 const Login = () => {
-  return <div>Login page</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="login-container">
+      <form onSubmit={(e) => handleLogin(e)}>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </section>
+  );
 };
 export default Login;
