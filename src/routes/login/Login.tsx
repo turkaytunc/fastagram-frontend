4e954a7e83c7743d2c294a9621029cd4779707a5
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-22 21:18:01 +0300

    add: joi validator for login inputs

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './login.scss';
+import { loginValidator } from 'src/helpers/joiValidators';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [inputError, setInputError] = useState('');
 
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    try {
+      await loginValidator(username, password);
+    } catch (error) {
+      setInputError(error.message);
+    }
   };
 
   return (
@@ -16,14 +24,17 @@ const Login = () => {
           type="text"
           placeholder="username"
           onChange={(event) => setUsername(event.target.value)}
+          onFocus={() => setInputError('')}
         />
         <input
           type="password"
           placeholder="password"
           onChange={(event) => setPassword(event.target.value)}
+          onFocus={() => setInputError('')}
         />
         <button type="submit">Login</button>
       </form>
+      {inputError}
     </section>
   );
 };
