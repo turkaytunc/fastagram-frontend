e692e8955c8d67578d8bcce2b44668872cbdd1e2
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-22 20:32:09 +0300

    update: routes, add 404 page for unhandled routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './app.scss';
+import Dashboard from './routes/dashboard/Dashboard';
 
 const Login = lazy(() => import('src/routes/login/Login'));
 const Register = lazy(() => import('src/routes/register/Register'));
 
 function App() {
   return (
-    <BrowserRouter basename="fastagram">
-      <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter basename="/fastagram">
+      <Suspense fallback={<div className="text-center">Loading...</div>}>
         <div className="app-container">
-          Hello
           <Switch>
+            <Route exact path="/" component={Dashboard} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
+            <Route exact path="/profile/:id" component={() => <div>profile</div>} />
+            <Route path="*" component={() => <div>404</div>} />
           </Switch>
         </div>
       </Suspense>
