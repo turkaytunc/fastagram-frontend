4d370ab4d3b03102b2ad929a305d8e8f16b7d1c0
Türkay Tunç <turkaytunc00@gmail.com>
2021-04-23 21:02:23 +0300

    update: file upload icon and style

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+/* eslint-disable dot-notation */
+import React, { useEffect, useState } from 'react';
 import FileUpload from 'src/components/file-upload/FileUpload';
 
 const Dashboard = () => {
   const [files, setFiles] = useState([]);
+
   return (
     <div>
-      <FileUpload multiple={false} onDone={setFiles} iconSize="30" fill="#333" />
+      <div className="w-6">
+        <FileUpload multiple={false} onDone={setFiles} iconSize="30" fill="#333" />
+      </div>
       <div className="text-center">
         {files?.map((file: any) => {
           return <img key={Math.random()} src={file.base64} alt="deneme" />;
