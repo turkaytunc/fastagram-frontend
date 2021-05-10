ddae116b4ba88b8896cec98c967bcd6e3a390f83
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-10 14:21:48 +0300

    add: signup page

--- src/api/signup/signupUser.ts
+++ src/api/signup/signupUser.ts
@@ -1,11 +1,12 @@
 import { BASE_URL } from 'src/constants';
 
-export const validateUser = async () =>
-  window.fetch(`${BASE_URL}/dashboard`, {
+export const signupUser = async (username: string, email: string, password: string) =>
+  window.fetch(`${BASE_URL}/auth/register`, {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json;charset=UTF-8',
     },
     credentials: 'include',
+    body: JSON.stringify({ username, email, password }),
   });
