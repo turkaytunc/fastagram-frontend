f25c417ebde6fb059eef1a4f4be2aaa541022fe8
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-10 13:28:54 +0300

    update: user context, now it provides provider and children in it. not context itself

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,6 +1,14 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import { User } from 'src/interfaces';
 
-const UserContext = createContext<null | User>(null);
+export const UserContext = createContext<null | { user: User; setUser: (user: User) => void }>(
+  null
+);
 
-export default UserContext;
+const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState({} as User);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export default UserContextProvider;
