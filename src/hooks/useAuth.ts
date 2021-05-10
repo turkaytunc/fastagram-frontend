1e3b36fecfcb50367f1d4d509d90d1971564e825
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-10 14:22:17 +0300

    add: custom hook for user validation

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from 'react';
+import { validateUser } from 'src/api';
+import { UserContext } from 'src/context/UserContext';
+import { User } from 'src/interfaces';
+import { useHistory } from 'react-router-dom';
+
+export default function useAuth() {
+  const userContext = useContext(UserContext);
+  const history = useHistory();
+  const [err, setErr] = useState('');
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await validateUser();
+        const data = await response.json();
+
+        if (!data.message) {
+          userContext?.setUser(data);
+          history.push('/');
+          return;
+        }
+        userContext?.setUser(null);
+        history.push('/login');
+      } catch (error) {
+        userContext?.setUser(null);
+        history.push('/login');
+        setErr('User validation failed');
+      }
+    };
+    fetchUser();
+  }, []);
+
+  return [err];
+}
