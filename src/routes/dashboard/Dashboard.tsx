db7be2e3ae6c3012c81e32d21eea25adf411508f
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-11 15:42:13 +0300

    add: navbar icons and style on dashboard

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable dot-notation */
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Navbar } from 'src/components';
 import FileUpload from 'src/components/file-upload/FileUpload';
 import useAuth from 'src/hooks/useAuth';
 
@@ -7,16 +8,21 @@ const Dashboard = () => {
   const [files, setFiles] = useState([]);
   const [err] = useAuth();
 
+  useEffect(() => {
+    document.title = 'Instagram';
+  }, []);
+
   return (
-    <div>
-      <div className="w-6">
+    <div className="dashboard-container">
+      <Navbar />
+      {/* <div className="w-6">
         <FileUpload multiple onDone={setFiles} iconSize="30" fill="#333" />
       </div>
       <div className="flex justify-center">
         {files?.map((file: any) => {
           return <img key={Math.random()} width="300px" src={file.base64} alt="deneme" />;
         })}
-      </div>
+      </div> */}
     </div>
   );
 };
