b4ec26095beba53faba0fed97f633057ef467556
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-16 11:28:59 +0300

    add: lcov coverage report type to jest config

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import App from '../App';
 
 it('should render without crashing', async () => {
-  render(<App />);
+  const history = createBrowserHistory();
+  history.push('/');
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
 });
