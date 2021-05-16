dc39f8a5e7196867ad52b9a1d0a036b41730565e
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-16 11:49:25 +0300

    add: tests for lazy loaded components in app.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,14 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import App from '../App';
 
-it('should render without crashing', async () => {
-  const history = createBrowserHistory();
-  history.push('/');
-  render(
-    <Router history={history}>
-      <App />
-    </Router>
-  );
+jest.spyOn(window, 'fetch');
+const fetch = window.fetch as jest.Mock;
+afterEach(cleanup);
+
+describe('<App/>', () => {
+  it('should render component without crashing', async () => {
+    const history = createBrowserHistory();
+    history.push('/fkldsjajf');
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(await screen.findByText('404')).toBeInTheDocument();
+  });
+
+  it('should render and lazy load dashboard component without crashing', async () => {
+    fetch.mockResolvedValue(() => ({ status: 200, json: () => ({}) }));
+    const history = createBrowserHistory();
+    history.push('/');
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(await screen.findByText('Log in')).toBeInTheDocument();
+  });
+  it('should render component and lazy load login component without crashing', async () => {
+    const history = createBrowserHistory();
+    history.push('/login');
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(await screen.findByText('Log in')).toBeInTheDocument();
+  });
+  it('should render component and lazy load signup component without crashing', async () => {
+    const history = createBrowserHistory();
+    history.push('/signup');
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    expect(await screen.findByText('Sign up')).toBeInTheDocument();
+  });
 });
