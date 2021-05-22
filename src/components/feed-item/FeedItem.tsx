415675e7715637e843afb037a2e0900399341041
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-22 16:43:53 +0300

    add: feed

--- src/components/feed-item/FeedItem.tsx
+++ src/components/feed-item/FeedItem.tsx
@@ -1,19 +1,25 @@
-import { Comment, Likes } from 'src/components';
+import { Comment, Heart } from 'src/components';
 import './feed-item.scss';
 
-const FeedItem = ({ username, likeCount }: { likeCount: string; username: string }) => {
+const FeedItem = ({
+  userId,
+  photoId,
+  imageData,
+}: {
+  imageData: string;
+  userId: string;
+  photoId: string;
+}) => {
   return (
     <div className="feed-item-container">
-      <header>{username}</header>
+      <header>
+        user:{userId} <p>photo: {photoId}</p>
+      </header>
       <div className="feed-item-image">
-        <img
-          width="400"
-          src="https://i.pinimg.com/originals/35/26/fc/3526fca5af9eaa620475a131a62cf1e6.jpg"
-          alt="anime girl"
-        />
+        <img width="400" src={imageData} alt="anime girl" />
       </div>
-      <Likes color="red" size="30" />
-      <div style={{ textAlign: 'start', marginTop: '0.2rem' }}>{likeCount} like</div>
+      <Heart size="30" />
+      <div style={{ textAlign: 'start', marginTop: '0.2rem', padding: '0.5rem' }}>{300} likes</div>
       <Comment />
     </div>
   );
