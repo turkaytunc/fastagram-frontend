3c35314be8d875307ba9cae24d9b10b41474bc0a
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-23 19:56:53 +0300

    refactor: test codes

--- src/__tests__/components/PhotosContainer.test.tsx
+++ src/__tests__/components/PhotosContainer.test.tsx
@@ -1,14 +1,79 @@
 import { screen, render } from '@testing-library/react';
 import { PhotosContainer } from 'src/components';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 describe('<PhotosContainer />', () => {
   it('should render without crashing', async () => {
-    render(<PhotosContainer photos={[{ id: '324', data: 'j234h2g34' }]} />);
+    const history = createBrowserHistory();
+    const userId = '487j-fdslkj23-fds3j34j';
+    history.push(`/profile/${userId}`);
+    render(
+      <Router history={history}>
+        <PhotosContainer userId={userId} />
+      </Router>
+    );
     expect(await screen.findByRole('img')).toBeInTheDocument();
   });
 
   it('should render without empty id', async () => {
-    render(<PhotosContainer photos={[{ id: '', data: 'j234h2g34' }]} />);
+    const history = createBrowserHistory();
+    const userId = '487j-fdslkj23-fds3j34j';
+    history.push(`/profile/${userId}`);
+
+    render(
+      <Router history={history}>
+        <PhotosContainer userId={userId} />
+      </Router>
+    );
     expect(await screen.findByRole('img')).toBeInTheDocument();
+  });
+
+  it('should fetch photos', async () => {
+    jest.spyOn(window, 'fetch');
+    (window.fetch as jest.Mock).mockResolvedValue({
+      status: 200,
+      json: () => ({
+        photos: [
+          { id: 1, data: 'jkfdsjfds' },
+          { id: 22, data: 'fdskjf' },
+        ],
+      }),
+    });
+
+    const history = createBrowserHistory();
+    const userId = '487j-fdslkj23-fds3j34j';
+    history.push(`/profile/${userId}`);
+
+    render(
+      <Router history={history}>
+        <PhotosContainer userId={userId} />
+      </Router>
+    );
+  });
+
+  it('should fail to fetch photos', async () => {
+    jest.spyOn(window, 'fetch');
+    (window.fetch as jest.Mock).mockResolvedValue({
+      status: 400,
+      json: () => ({
+        photos: [
+          { id: 1, data: 'jkfdsjfds' },
+          { id: 22, data: 'fdskjf' },
+        ],
+      }),
+    });
+
+    const history = createBrowserHistory();
+    const userId = '487j-fdslkj23-fds3j34j';
+    history.push(`/profile/${userId}`);
+
+    render(
+      <Router history={history}>
+        <PhotosContainer userId={userId} />
+      </Router>
+    );
+
+    expect(await screen.findByText('Cannot fetch data')).toBeTruthy();
   });
 });
--- src/__tests__/routes/Profile.test.tsx
+++ src/__tests__/routes/Profile.test.tsx
@@ -1,74 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Profile from 'src/routes/profile/Profile';
+import { UserContextProvider } from 'src/context/UserContext';
 
 jest.spyOn(window, 'fetch');
 
 describe('<Profile />', () => {
-  it('should fetch profile and photos without crashing', async () => {
-    (window.fetch as jest.Mock).mockResolvedValue({
-      status: 200,
-      json: () => ({
-        photos: [
-          { id: 1, data: 'jkfdsjfds' },
-          { id: 22, data: 'fdskjf' },
-        ],
-        profile: { username: 'turkay234', fullname: 'turkay tunc', email: 'turk@gmail.com' },
-      }),
-    });
-
+  it('should without crashing', async () => {
     const history = createBrowserHistory();
     const userId = '487j-fdslkj23-fds3j34j';
     history.push(`/profile/${userId}`);
 
     render(
       <Router history={history}>
-        <Profile />
+        <UserContextProvider>
+          <Profile />
+        </UserContextProvider>
       </Router>
     );
-
-    expect(await screen.findByText(/turkay234/)).toBeInTheDocument();
-  });
-
-  it('should fetch profile but get server error', async () => {
-    (window.fetch as jest.Mock).mockResolvedValue({
-      status: 400,
-      json: () => ({
-        photos: [
-          { id: 1, data: 'jkfdsjfds' },
-          { id: 22, data: 'fdskjf' },
-        ],
-        profile: { username: 'turkay234', fullname: 'turkay tunc', email: 'turk@gmail.com' },
-      }),
-    });
-
-    const history = createBrowserHistory();
-    const userId = '487j-fdslkj23-fds3j34j';
-    history.push(`/profile/${userId}`);
-
-    render(
-      <Router history={history}>
-        <Profile />
-      </Router>
-    );
-
-    expect(await screen.findByText(/Cannot fetch data/)).toBeInTheDocument();
-  });
-
-  it('should not fetch profile and get error', async () => {
-    (window.fetch as jest.Mock).mockRejectedValue(new Error('Something went wrong!'));
-
-    const history = createBrowserHistory();
-    const userId = '487j-fdslkj23-fds3j34j';
-    history.push(`/profile/${userId}`);
-
-    render(
-      <Router history={history}>
-        <Profile />
-      </Router>
-    );
-
-    expect(await screen.findByText(/Something went wrong!/)).toBeInTheDocument();
   });
 });
