2e3463aee746be7fef05a1f819931e9770193883
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-27 10:33:11 +0300

    update: comments display area styles

--- src/components/comment-item/CommentItem.tsx
+++ src/components/comment-item/CommentItem.tsx
@@ -5,16 +5,23 @@ const CommentItem = ({
   comment: { id: string; photo_id: string; user_id: string; content: string; created_at: string };
 }) => {
   return (
-    <section>
+    <>
       {comment.created_at && (
-        <>
-          <section style={{ fontSize: '0.7rem', textAlign: 'start', padding: '0.2rem 1rem' }}>
-            {new Date(comment?.created_at).toLocaleDateString()}
-          </section>
-          <section style={{ textAlign: 'start', paddingLeft: '2rem' }}>{comment.content}</section>
-        </>
+        <section style={{ display: 'flex', paddingLeft: '0.5rem' }}>
+          {comment.content}
+          <span
+            style={{
+              fontSize: '0.7rem',
+              textAlign: 'start',
+              padding: '0.2rem 1rem',
+              color: '#aaa',
+            }}
+          >
+            on {new Date(comment?.created_at).toLocaleDateString()}
+          </span>
+        </section>
       )}
-    </section>
+    </>
   );
 };
 
