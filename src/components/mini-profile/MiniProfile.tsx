e980118e668bc3d043b9f68299a1ad5249fb1c0e
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-27 10:18:23 +0300

    update: email and username display

--- src/components/mini-profile/MiniProfile.tsx
+++ src/components/mini-profile/MiniProfile.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
+import { FaBolt } from 'react-icons/fa';
 import useProfile from 'src/hooks/useProfile';
 import './mini-profile.scss';
 
 const MiniProfile = ({ userId }: { userId: string }) => {
   const [profile, profileFetchError] = useProfile(userId);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       {profileFetchError === '' ? (
         <div className="mini-profile">
           <section className="mini-username">{profile?.username}</section>
-          <section>
-            <section className="mini-fullname">{profile?.fullname}</section>
-            <section className="mini-email">{profile?.email}</section>
-          </section>
+
+          {isOpen && (
+            <section className="mini-profile-menu">
+              <section className="mini-fullname">{profile?.fullname}</section>
+              <section className="mini-email">{profile?.email}</section>
+            </section>
+          )}
+          <button type="button" onClick={() => setIsOpen((prev) => !prev)}>
+            <FaBolt fill="#666" />
+          </button>
         </div>
       ) : (
         <div>{profileFetchError}</div>
