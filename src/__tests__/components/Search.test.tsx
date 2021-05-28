b11e820807ccaa27268caf62bca8e746acfb4ea6
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-28 14:10:15 +0300

    add: search component tests

--- src/__tests__/components/Search.test.tsx
+++ src/__tests__/components/Search.test.tsx
@@ -1,14 +1,88 @@
 import { screen, render, fireEvent } from '@testing-library/react';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import { Search } from 'src/components';
 
+const history = createBrowserHistory();
+jest.spyOn(window, 'fetch');
+const mockFetch = window.fetch as jest.Mock;
 describe('<Search />', () => {
   it('should fire change event', async () => {
-    render(<Search />);
+    history.push('/');
+    mockFetch.mockResolvedValue({
+      status: 200,
+      json: () => ({
+        users: [{ username: 'enes', user_id: '' }],
+      }),
+    });
+    render(
+      <Router history={history}>
+        <Search />
+      </Router>
+    );
 
     fireEvent.change(await screen.findByRole('searchbox'), {
       target: { value: 'searching things' },
     });
 
     expect(await screen.findByRole('searchbox')).toHaveValue('searching things');
+  });
+
+  it('should fire change event and get error', async () => {
+    history.push('/');
+    mockFetch.mockRejectedValue(new Error('Something went wrong'));
+    render(
+      <Router history={history}>
+        <Search />
+      </Router>
+    );
+
+    fireEvent.change(await screen.findByRole('searchbox'), {
+      target: { value: 'searching things' },
+    });
+
+    expect(await screen.findByRole('searchbox')).toHaveValue('searching things');
+  });
+
+  it('should fire change event and fetch error message from server', async () => {
+    history.push('/');
+    mockFetch.mockResolvedValue({
+      status: 400,
+      json: () => ({ message: 'Some server problem message' }),
+    });
+    render(
+      <Router history={history}>
+        <Search />
+      </Router>
+    );
+
+    fireEvent.change(await screen.findByRole('searchbox'), {
+      target: { value: 'searching things' },
+    });
+
+    expect(await screen.findByRole('searchbox')).toHaveValue('searching things');
+  });
+
+  it('should fire change event', async () => {
+    history.push('/');
+    mockFetch.mockResolvedValue({
+      status: 200,
+      json: () => ({
+        users: [{ username: 'onur', user_id: '423j-423kl-jh21j' }],
+      }),
+    });
+    render(
+      <Router history={history}>
+        <Search />
+      </Router>
+    );
+
+    fireEvent.change(await screen.findByRole('searchbox'), {
+      target: { value: 'searching things' },
+    });
+
+    fireEvent.click(await screen.findByRole('button'));
+
+    expect(window.location.pathname).toBe('/profile/423j-423kl-jh21j');
   });
 });
